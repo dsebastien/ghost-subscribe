5e84c869b296f12b5445eb874c099ef9abb434b9
Sebastien Dubois <seb@dsebastien.net>
2024-05-15 16:23:25 +0200

    Added script and example

---
+++ ghost-subscribe.js
@@ -0,0 +1,29 @@
+async function subscribeToGhost(email, ghostSiteBaseUrl) {
+    const emailSentElement = document.getElementById('subscription-mail-sent');
+    const subscriptionFormElement = document.getElementById('subscription-form');
+
+    try {
+        const result = await fetch(`${ghostSiteBaseUrl}/members/api/send-magic-link/`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({ 
+                email,
+                emailType: "subscribe",
+            }),
+        });
+
+        // TODO handle failure scenario
+        // const resultText = await result.text();
+        //if ("Created." !== resultText){
+        //}
+    } catch (e) {
+        console.log("Failed to subscribe", e);
+    } finally {
+        if(emailSentElement && subscriptionFormElement) {
+            subscriptionFormElement.classList.add('hidden');
+            emailSentElement.classList.remove('invisible');
+        }
+    }
+}
